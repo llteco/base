7fdcd155f00e5423b8732f9f1d7cd3779ec7a22a
Wenyi Tang <wenyi.tang@intel.com>
2019-09-25 11:16:16 +0800

    Add patch to imgui to enable uint32 indice type

--- cmake/imgui.cmake
+++ cmake/imgui.cmake
@@ -1,8 +1,15 @@
+# Copyright (c) 2019 Wenyi Tang
+# Author: Wenyi Tang
+# E-mail: wenyitang@outlook.com
 include (ExternalProject)
 
 set(imgui_URL https://github.com/ocornut/imgui.git)
-set(imgui_TAG v1.60) # 1.6.0
+set(imgui_TAG v1.60)
 set(imgui_INCLUDE_DIRS ${CMAKE_CURRENT_BINARY_DIR}/imgui/include)
+
+if(NOT EXISTS ${LL_SCRIPT_ROOT})
+  set(LL_SCRIPT_ROOT ${CMAKE_SOURCE_DIR}/scripts/cmake)
+endif()
 
 if(WIN32)
   set(imgui_STATIC_LIBS
@@ -20,11 +27,14 @@ ExternalProject_Add(imgui-src
   PREFIX imgui
   GIT_REPOSITORY ${imgui_URL}
   GIT_TAG ${imgui_TAG}
-  PATCH_COMMAND ${CMAKE_COMMAND} -E copy_if_different ...
-    ${CMAKE_SOURCE_DIR}/cmake/patches/imgui/CMakeLists.txt <SOURCE_DIR>
+  PATCH_COMMAND 
+    ${CMAKE_COMMAND} -E copy_if_different
+        ${LL_SCRIPT_ROOT}/patches/imgui/CMakeLists.txt <SOURCE_DIR>
+    && ${CMAKE_COMMAND} -E copy
+        ${LL_SCRIPT_ROOT}/patches/imgui/imconfig.h <SOURCE_DIR>
   CMAKE_CACHE_ARGS
     -DIMGUI_BUILD_EXAMPLES:BOOL=ON
-    -DCMAKE_DEBUG_POSTFIX:STRING=d
+    -DCMAKE_DEBUG_POSTFIX:STRING=${CMAKE_DEBUG_POSTFIX}
     -DCMAKE_CXX_FLAGS_DEBUG:STRING=${CMAKE_CXX_FLAGS_DEBUG}
     -DCMAKE_CXX_FLAGS_RELEASE:STRING=${CMAKE_CXX_FLAGS_RELEASE}
     -DCMAKE_INSTALL_PREFIX:STRING=<INSTALL_DIR>)
--- cmake/spdlog.cmake
+++ cmake/spdlog.cmake
@@ -1,7 +1,10 @@
+# Copyright (c) 2019 Wenyi Tang
+# Author: Wenyi Tang
+# E-mail: wenyitang@outlook.com
 include(ExternalProject)
 
 set(spdlog_URL https://github.com/gabime/spdlog.git)
-set(spdlog_TAG v1.3.1)
+set(spdlog_TAG v1.4.1)
 set(spdlog_INCLUDE_DIRS ${CMAKE_CURRENT_BINARY_DIR}/spdlog/include)
 
 ExternalProject_Add(spdlog-src
