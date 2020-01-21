571a620fc168e05b66cebbb4f290ce39975ab046
Wenyi Tang <wenyi.tang@intel.com>
2020-01-21 20:52:47 +0800

    Update format script

--- tools/format_all.py
+++ tools/format_all.py
@@ -3,9 +3,11 @@
 # @Author: Wenyi Tang
 # @Email: wenyi.tang@intel.com
 
-from pathlib import Path
-import subprocess
 import argparse
+import re
+import subprocess
+from pathlib import Path
+
 
 def CollectFilesFromDir(path, recurse=False):
     """Glob all files (recursively) in the given path"""
@@ -26,10 +28,21 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('dir')
     parser.add_argument('--formatter', default='clang-format')
-    parser.add_argument('--style', default='Google')
+    parser.add_argument('--style', default='file')
+    parser.add_argument('--exclude', nargs='*', default=[])
     param = parser.parse_args()
 
+    def exclude_file(x):
+        x = Path(x).resolve().absolute()
+        if not x.exists():
+            return False
+        for expat in param.exclude:
+            if re.findall(expat, str(x)):
+                return False
+        return True
+
     files = CollectFilesFromDir(param.dir, True)
-    for f in files:
+    for f in filter(exclude_file, files):
         cmd = f"{param.formatter} -style={param.style} -i {str(f)}"
+        cmd += " --fallback-style=google"
         subprocess.call(cmd, stdout=None, shell=True)
